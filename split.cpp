245ae15af5fe4d309d23b8945b1ff5414901008c
Caleb Swan <ciswan@usc.edu>
2025-09-06 08:27:10 +0000

    done with part 1

--- split.cpp
+++ split.cpp
@@ -18,6 +18,34 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  //probably check if null (nothing to return?)
+  if (in == nullptr){
+    odds = nullptr;
+    evens = nullptr;
+    return;
+  } 
+
+  //we need to go from the back to the front first
+  Node *curr = in;
+  Node *temp = curr->next;
+  curr->next = nullptr;
+  split(temp, odds, evens);
+
+  //check if even
+  if (curr->value % 2 == 0){
+    curr->next = evens;
+    evens = curr;
+  }
+
+  //check if odd
+  else {
+    curr->next = odds;
+    odds = curr;
+  }
+
+  //finally set `in` to nullptr
+  in = nullptr;
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,57 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+
+  //setup
+  Node *test = new Node(0, nullptr);
+  Node *tail = test;
+  for (int i = 1; i <= 10; i++){
+    tail->next = new Node(i, nullptr);
+    tail = tail->next;
+  }
+
+  Node *odds = nullptr;
+  Node *evens = nullptr;
+
+  split(test,odds,evens);
+
+  Node *counter1 = evens;
+  Node *counter2 = odds;
+
+  std::cout << "testing with a linked list from 1-10: " << std::endl;
+
+  std::cout << "evens: ";
+  while (counter1 != nullptr){
+    std::cout << counter1->value << " ";
+    counter1 = counter1->next;
+  }
+  std::cout << std::endl;
+
+  std::cout << "odds: ";
+  while (counter2 != nullptr){
+    std::cout << counter2->value << " ";
+    counter2 = counter2->next;
+  }
+  std::cout << std::endl;
+
+  //delete
+  counter1 = evens;
+  counter2 = odds;
+  while (counter1 != nullptr){
+    Node *temp = counter1->next;
+    delete counter1;
+    counter1 = temp;
+  }
+
+  while (counter2 != nullptr){
+    Node *temp = counter2->next;
+    delete counter2;
+    counter2 = temp;
+  }
+  
 
 }
